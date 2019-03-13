1c5e57ddc45a6a0713e1f5fc2d9d5e2abe3b6a08
stevenshuang <stevenshuang521@gmail.com>
2019-03-14 01:03:03 +0800

    MinimumDepthofBinaryTree

---
+++ zs-leetcode-go/MinimumDepthofBinaryTree111/MinimumDepthofBinaryTree.go
@@ -0,0 +1,18 @@
+package MinimumDepthofBinaryTree111
+
+import "for-an-offer/zs-leetcode-go/types"
+
+func  MinimumDepthofBinaryTree(root *types.TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	left := MinimumDepthofBinaryTree(root.Left)
+	right := MinimumDepthofBinaryTree(root.Right)
+	if left == 0 || right == 0 {
+		return  left + right + 1
+	}
+	if left < right {
+		return left + 1
+	}
+	return right + 1
+}
