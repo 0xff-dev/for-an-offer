1608604f220c920a46003d5489296f1cb74d7af6
stevenshuang <stevenshuang521@gmail.com>
2019-03-21 00:04:31 +0800

    subsets test

---
+++ zs-leetcode-go/Subsets/subsets_test.go
@@ -0,0 +1,17 @@
+package Subsets
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestSubsets(t *testing.T) {
+	testDatas := [][]int{
+		[]int{1,2,3},
+		[]int{3,2,1},
+		[]int{4,1,2,5},
+	}
+	for _, data := range testDatas {
+		fmt.Println(subsets(data))
+	}
+}
