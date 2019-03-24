266d07fc50a29be1a8bd6ce68465ec15c9ffcc30
stevenshuang <stevenshuang521@gmail.com>
2019-03-24 12:39:54 +0800

    CombinationSum test

---
+++ zs-leetcode-go/CombinationSum/CombinationSum_test.go
@@ -0,0 +1,13 @@
+package CombinationSum
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestCombinationSum(t *testing.T) {
+	candidates := []int{2,3,6,7}
+	fmt.Println(combinationSum(candidates, 7))
+	candidates = []int{2,3,5}
+	fmt.Println(combinationSum(candidates, 8))
+}
