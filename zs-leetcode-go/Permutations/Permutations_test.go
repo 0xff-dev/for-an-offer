2c54c5b2733369c01ffce2b381953fc33c993210
stevenshuang <stevenshuang521@gmail.com>
2019-03-26 19:51:31 +0800

    Permutations test

---
+++ zs-leetcode-go/Permutations/Permutations_test.go
@@ -0,0 +1,16 @@
+package Permutations
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestPermute(t *testing.T) {
+	datas := [][]int{
+		[]int{1,2,3},
+		[]int{1,4,5},
+	}
+	for _, data := range datas {
+		fmt.Println(permute(data))
+	}
+}
