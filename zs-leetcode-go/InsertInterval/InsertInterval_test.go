25ec4d06634c47d8b08142f0fa39349a89be654e
stevenshuang <stevenshuang521@gmail.com>
2019-03-27 00:07:50 +0800

    InsertInterval test

---
+++ zs-leetcode-go/InsertInterval/InsertInterval_test.go
@@ -0,0 +1,33 @@
+package InsertInterval
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestInsert(t *testing.T) {
+	testDatas := []Interval {
+		{1,3},
+		{6,9},
+	}
+	fmt.Println(insert(testDatas, Interval{2,5}))
+	testDatas = []Interval {
+		{1,2},
+		{3,5},
+		{6,7},
+		{8, 10},
+		{12, 16},
+	}
+	fmt.Println(insert(testDatas, Interval{4,8}))
+
+	testDatas = []Interval{}
+	fmt.Println(insert(testDatas, Interval{5,7}))
+
+	testDatas = []Interval{{1,5}}
+	fmt.Println(insert(testDatas, Interval{4,8}))
+
+	testDatas = []Interval{
+		{1,5},
+	}
+	fmt.Println(insert(testDatas, Interval{6,8}))
+}
