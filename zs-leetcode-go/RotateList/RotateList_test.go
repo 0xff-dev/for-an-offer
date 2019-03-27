0f70347e285da14f3fa41fa52f5ccb9a634a0823
stevenshuang <stevenshuang521@gmail.com>
2019-03-27 14:02:12 +0800

    RotateList test

---
+++ zs-leetcode-go/RotateList/RotateList_test.go
@@ -0,0 +1,23 @@
+package RotateList
+
+import (
+	"for-an-offer/zs-leetcode-go/types"
+	"testing"
+)
+
+func TestRotateRight(t *testing.T) {
+	list := &types.ListNode{1, &types.ListNode{2, &types.ListNode{3,
+	&types.ListNode{4, &types.ListNode{5, nil}}}}}
+	root := rotateRight(list, 2)
+	root.Display()
+
+	list = &types.ListNode{0, &types.ListNode{1, &types.ListNode{2, nil}}}
+	root = rotateRight(list, 4)
+	root.Display()
+
+	list = &types.ListNode{0, nil}
+	root = rotateRight(list, 2)
+	root.Display()
+
+	list = &types.ListNode{}
+}
