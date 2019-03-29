07778ed0b5e7ceefcf12eedd9ef89da08bb6bbdc
stevenshuang <stevenshuang521@gmail.com>
2019-03-29 12:13:11 +0800

    SortColors test

---
+++ zs-leetcode-go/SortColors/SortColors_test.go
@@ -0,0 +1,23 @@
+package SortColors
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestSortColors(t *testing.T) {
+	testDatas := [][]int{
+		{2,0,2,1,1,0},
+		{1,0,0,1,2},
+		{0},
+		{2,1},
+		{2,1,0,0,2,1},
+		{1,2,0},
+		{1,0,0,0,0,1,1,2,1},
+		{2,2,2,1,0,0,0,2,1},
+	}
+	for _, data := range testDatas {
+		sortColors(data)
+		fmt.Println(data)
+	}
+}
