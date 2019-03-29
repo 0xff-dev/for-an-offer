baf6d66e650d9414503829e727bd2d46296b26df
stevenshuang <stevenshuang521@gmail.com>
2019-03-29 12:11:27 +0800

    SortColors O(n), constant space

---
+++ zs-leetcode-go/SortColors/SortColors.go
@@ -0,0 +1,19 @@
+// 快排思想， 1做基准
+package SortColors
+
+
+func sortColors(nums []int) {
+	length := len(nums)
+	start, end := 0, length-1
+	index := 0
+	for ;index <= end; index++ {
+		if nums[index] == 2 {
+			nums[end], nums[index] = nums[index], nums[end]
+			end--
+			index--
+		} else if nums[index] == 0 {
+			nums[start], nums[index] = nums[index], nums[start]
+			start++
+		}
+	}
+}
