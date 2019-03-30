ce024a0295490db6c749c203ef9dc39e1cb6f5e1
stevenshuang <stevenshuang521@gmail.com>
2019-03-30 08:42:28 +0800

    RemoveDuplicatesII

---
+++ zs-leetcode-go/RemoveDuplicatesfromSortedArrayII/RemoveDuplicatesII.go
@@ -0,0 +1,28 @@
+//  O(1)的额外空间是在原数组修改， 那么我们就要对数组进行移动..
+package RemoveDuplicatesfromSortedArrayII
+
+
+func removeDuplicates(nums []int) int {
+	length := len(nums)
+	for index := 0; index < length-1; index++ {
+		cnt := 1
+		for j := index+1; j < length; j++ {
+			if nums[j] == nums[index] {
+				cnt += 1
+			} else {
+				break
+			}
+		}
+		if cnt > 2 {
+			steps := cnt - 2
+			start := index+2
+			for k := index+cnt; k < length; k, start = k+1, start+1 {
+				nums[start] = nums[k]
+			}
+			length -= steps
+			index++
+		}
+	}
+	nums = nums[:length]
+	return length
+}
