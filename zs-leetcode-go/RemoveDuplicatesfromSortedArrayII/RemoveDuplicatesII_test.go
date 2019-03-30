1599df600cd20e2661d3239b89a4ea1ea17e39cc
stevenshuang <stevenshuang521@gmail.com>
2019-03-30 08:42:41 +0800

    RemoveDuplicatesII test

---
+++ zs-leetcode-go/RemoveDuplicatesfromSortedArrayII/RemoveDuplicatesII_test.go
@@ -0,0 +1,19 @@
+package RemoveDuplicatesfromSortedArrayII
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRemoveDuplicates(t *testing.T) {
+	testDatas := [][]int{
+		{1,1,1,2,2,3},
+		{0,0,1,1,1,1,2,3,3},
+		{1,2,3},
+		{1,1,2,2,3,3},
+	}
+	for _, item := range testDatas {
+		fmt.Println(removeDuplicates(item))
+		fmt.Println(item)
+	}
+}
