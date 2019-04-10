a29afa7e03f94af10837ff7ca2c1f6075ab2e675
stevenshuang <stevenshuang521@gmail.com>
2019-04-10 12:06:27 +0800

    LRUCache test

---
+++ zs-leetcode-go/LRUCache/LRUCache_test.go
@@ -0,0 +1,35 @@
+package LRUCache
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestFunc(t *testing.T) {
+	root := Constructor(2)
+	root.Put(1,1)
+	root.Put(2,2)
+	fmt.Println(root.Get(1))
+	root.Put(3,3)
+	fmt.Println(root.Get(2))
+	root.Put(4,4)
+	fmt.Println(root.Get(1))
+	fmt.Println(root.Get(3))
+	fmt.Println(root.Get(4))
+
+	root = Constructor(2)
+	root.Put(2,1)
+	root.Put(1,1)
+	root.Put(2,3)
+	root.Put(4,1)
+	fmt.Println(root.Get(1))
+	fmt.Println(root.Get(2))
+
+	root = Constructor(2)
+	root.Put(2,6)
+	fmt.Println(root.Get(1))
+	root.Put(1,5)
+	root.Put(1,2)
+	fmt.Println(root.Get(1))
+	fmt.Println(root.Get(2))
+}
