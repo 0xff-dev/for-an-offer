2a43aabb2e8790a42a9960ff9cf06446101418f4
stevenshuang <stevenshuang521@gmail.com>
2019-04-24 18:37:56 +0800

    DivisorGame test

---
+++ zs-leetcode-go/DivisorGame/DivisorGame_test.go
@@ -0,0 +1,12 @@
+package DivisorGame
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestDivisorGame(t *testing.T) {
+	fmt.Println(divisorGame(2))
+	fmt.Println(divisorGame(3))
+	fmt.Println(divisorGame(5))
+}
