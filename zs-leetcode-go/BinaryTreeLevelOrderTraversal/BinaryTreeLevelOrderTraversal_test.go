a1e4d4fd58fc7cbce8ad181d1ac529940e001e1c
stevenshuang <stevenshuang521@gmail.com>
2019-05-04 15:25:34 +0800

    BinaryTreeLevelOrderTraversal test

---
+++ zs-leetcode-go/BinaryTreeLevelOrderTraversal/BinaryTreeLevelOrderTraversal_test.go
@@ -0,0 +1,14 @@
+package BinaryTreeLevelOrderTraversal
+
+import (
+	"fmt"
+	"for-an-offer/zs-leetcode-go/types"
+	"testing"
+)
+
+func TestLevelOrder(t *testing.T) {
+	tree := &types.TreeNode{Val:3, Left: &types.TreeNode{Val:9, Left:nil, Right:nil}, Right: nil}
+	tree.Right = &types.TreeNode{Val:20, Left:&types.TreeNode{Val:15, Left:nil, Right:nil},
+		Right:&types.TreeNode{Val:7, Left:nil, Right:nil}}
+	fmt.Println(levelOrder(tree))
+}
