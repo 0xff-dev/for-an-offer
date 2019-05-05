1c21525abcea0dfa972b2cd0274ac743b424d97d
stevenshuang <stevenshuang521@gmail.com>
2019-05-05 18:52:55 +0800

    ConstructBinaryTreefromInorderandPostorderTraversal

---
+++ zs-leetcode-go/ConstructBinaryTreefromInorderandPostorderTraversal/ConstructBinaryTreefromInorderandPostorderTraversal.go
@@ -0,0 +1,29 @@
+package ConstructBinaryTreefromInorderandPostorderTraversal
+
+import "for-an-offer/zs-leetcode-go/types"
+
+func buildTree(inorder []int, postorder []int) *types.TreeNode {
+	inorderLen, postorderLen := len(inorder), len(postorder)
+	if inorderLen == 0 {
+		return nil
+	}
+	root := &types.TreeNode{}
+	root.Val = postorder[postorderLen-1]
+	// 找到
+	if inorderLen == 1 {
+		if postorderLen == 1 && postorder[0] == inorder[0] {
+			return root
+		}
+		return nil
+	}
+	index := 0
+	for ; inorder[index] != postorder[postorderLen-1]; index++{}
+	// 能找到
+	if index > 0 {
+		root.Left = buildTree(inorder[:index], postorder[:index])
+	}
+	if index < inorderLen {
+		root.Right = buildTree(inorder[index+1:], postorder[index:postorderLen-1])
+	}
+	return root
+}
