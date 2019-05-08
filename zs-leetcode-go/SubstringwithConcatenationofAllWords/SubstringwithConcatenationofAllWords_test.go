f279a68b295a80ecc938f01af73aa2361e86156c
stevenshuang <stevenshuang521@gmail.com>
2019-05-08 20:01:09 +0800

    SubstringwithConcatenationofAllWords test

---
+++ zs-leetcode-go/SubstringwithConcatenationofAllWords/SubstringwithConcatenationofAllWords_test.go
@@ -0,0 +1,23 @@
+package SubstringwithConcatenationofAllWords
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestFindSubstring(t *testing.T) {
+	s := "barfoothefoobarman"
+	aim := []string{"bar", "foo"}
+	fmt.Println(findSubstring(s, aim))
+
+	s = "wordgoodgoodgoodbestword"
+	fmt.Println(findSubstring(s, []string{"word", "good", "best", "word"}))
+
+	s = "wordgoodgoodgoodbestword"
+	aim = []string{"word","good","best","good"}
+	fmt.Println(findSubstring(s, aim))
+
+	s = "lingmindraboofooowingdingbarrwingmonkeypoundcake"
+	aim = []string{"fooo","barr","wing","ding","wing"}
+	fmt.Println(findSubstring(s, aim))
+}
