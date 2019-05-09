78d0d3aa5a6fd0b0ccfc48a228fe38b2f036bb8d
stevenshuang <stevenshuang521@gmail.com>
2019-05-09 18:20:34 +0800

    FirstMissingPositive test

---
+++ zs-leetcode-go/FirstMissingPositive/FirstMissingPositive_test.go
@@ -0,0 +1,11 @@
+package FirstMissingPositive
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestFirstMissingPositive(t *testing.T) {
+	fmt.Println(firstMissingPositive([]int{1,2,0}))
+	fmt.Println(firstMissingPositive([]int{3,5,6,2,1}))
+}
