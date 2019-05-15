52d9ec8aa3426ee64afe0587223a0b45c18051df
stevenshuang <stevenshuang521@gmail.com>
2019-05-15 19:29:07 +0800

    WordSearch test

---
+++ zs-leetcode-go/WordSearch/WordSearch_test.go
@@ -0,0 +1,25 @@
+package WordSearch
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestExists(t *testing.T) {
+	board := [][]byte{
+		{'A', 'B', 'C', 'E'},
+		{'S', 'F', 'C', 'S'},
+		{'A', 'D', 'E', 'E'},
+	}
+	fmt.Println(exist(board, "ABCCED"))
+	fmt.Println(exist(board, "SEE"))
+	fmt.Println(exist(board, "ABCB"))
+	fmt.Println(exist([][]byte{}, "dfg"))
+
+	board = [][]byte{
+		{'A', 'B', 'C', 'E'},
+		{'S', 'F', 'E', 'S'},
+		{'A', 'D', 'E', 'E'},
+	}
+	fmt.Println(exist(board, "ABCESEEEFS"))
+}
