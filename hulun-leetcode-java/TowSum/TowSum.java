983f50ace0d4d0868143d9d70424d8cf523d59e9
hulunbao <hulunbao@gmail.com>
2019-05-16 15:10:15 +0800

    修改下测试

--- hulun-leetcode-java/TowSum/TowSum.java
+++ hulun-leetcode-java/TowSum/TowSum.java
@@ -7,7 +7,15 @@ public class TowSum {
         int[] a = new int[]{2,7,11,15};
         Solution solution = new Solution();
         result = solution.twoSum(a,9);
-        System.out.println(result);
+        System.out.print("[");
+        for(int i=0;i<result.length;i++){
+            System.out.print(result[i]);
+            if(i==1){
+                break;
+            }
+            System.out.print(",");
+        }
+        System.out.print("]");
     }
 }
 class Solution {
