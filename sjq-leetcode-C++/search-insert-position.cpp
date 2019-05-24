7a758c21b118766665bcb5c0f901149022c93fa4
sherley-shi <36828725+sherley-shi@users.noreply.github.com>
2019-05-24 20:07:37 +0800

    Create search-insert-position.cpp

---
+++ sjq-leetcode-C++/search-insert-position.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int i = 0;
+        for(i; i < nums.size(); i++){
+            if(nums[i] >= target)
+                return i;
+        }
+        return i;
+    }
+};
