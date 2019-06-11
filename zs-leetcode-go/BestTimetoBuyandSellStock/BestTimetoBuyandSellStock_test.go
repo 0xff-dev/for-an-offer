6d06261ce5464265436e13e7395ba03641088af7
stevenshuang <stevenshuang521@gmail.com>
2019-06-11 18:29:22 +0800

    BestTimetoBuyandSellStock test

---
+++ zs-leetcode-go/BestTimetoBuyandSellStock/BestTimetoBuyandSellStock_test.go
@@ -0,0 +1,11 @@
+package BestTimetoBuyandSellStock
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestMaxProfit(t *testing.T) {
+	fmt.Println(maxProfit([]int{7,1,5,3,6,4}))
+	fmt.Println(maxProfit([]int{7,6,4,3,1}))
+}
