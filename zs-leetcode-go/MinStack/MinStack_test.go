893e3765300932e0fd6a992e23230ee6611664ec
stevenshuang <stevenshuang521@gmail.com>
2019-06-22 21:39:38 +0800

    MinStack test

---
+++ zs-leetcode-go/MinStack/MinStack_test.go
@@ -0,0 +1,17 @@
+package MinStack
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestMinStack(t *testing.T) {
+	obj := Constructor()
+	obj.Push(-2)
+	obj.Push(0)
+	obj.Push(-3)
+	fmt.Println(obj.GetMin())
+	obj.Pop()
+	fmt.Println(obj.Top())
+	fmt.Println(obj.GetMin())
+}
