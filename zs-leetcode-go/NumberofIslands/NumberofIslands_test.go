b82a4571e5165aa5125c85ab0632a85e77114353
stevenshuang <stevenshuang521@gmail.com>
2019-07-07 10:07:12 +0800

    NumberofIslands test

---
+++ zs-leetcode-go/NumberofIslands/NumberofIslands_test.go
@@ -0,0 +1,23 @@
+package NumberofIslands
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestNumIslands(t *testing.T) {
+	data := [][]byte{
+		{'1', '1', '1', '1', '0'},
+		{'1', '1', '0', '1', '0'},
+		{'1', '1', '0', '0', '0'},
+		{'0', '0', '0', '0', '0'},
+	}
+	fmt.Println(numIslands(data))
+	data = [][]byte {
+		{'1', '1', '0', '0', '0'},
+		{'1', '1', '0', '0', '0'},
+		{'0', '0', '1', '0', '0'},
+		{'0', '0', '0', '1', '1'},
+	}
+	fmt.Println(numIslands(data))
+}
