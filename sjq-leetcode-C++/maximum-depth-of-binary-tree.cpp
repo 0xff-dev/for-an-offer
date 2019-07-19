b0014b9ff6e271189f70141aad83167a9d2b30cf
sherley-shi <36828725+sherley-shi@users.noreply.github.com>
2019-07-19 22:16:38 +0800

    Update maximum-depth-of-binary-tree.cpp

--- sjq-leetcode-C++/maximum-depth-of-binary-tree.cpp
+++ sjq-leetcode-C++/maximum-depth-of-binary-tree.cpp
@@ -11,7 +11,6 @@ class Solution {
 public:
     int maxDepth(TreeNode* root) {
         if(root == NULL) return 0;
-        if(root->left==NULL and root->right==NULL) return 1;
         return max(maxDepth(root->left), maxDepth(root->right))+1;
     }
 };
