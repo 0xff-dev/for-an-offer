2af86778015dc85f25656fdff3e7e0029f8ffd04
hulunbao <hulunbao@gmail.com>
2019-08-16 17:36:30 +0800

    add two number

--- hulun-leetcode-java/AddTwoNumber/AddTwoNumber.java
+++ hulun-leetcode-java/AddTwoNumber/AddTwoNumber.java
@@ -1,3 +1,5 @@
+import org.w3c.dom.NodeList;
+
 public class AddTwoNumber{
     /*
 2 -> 4 -> 3
@@ -12,7 +14,7 @@ public class AddTwoNumber{
         ListNode p2 = l2;
 
         int carry = 0;
-        while(p1!=null || p2!=null){
+        while(p1!=null || p2!=null || carry > 0){
             int sum = carry;
             if(p1!=null){
                 sum += p1.val;
@@ -24,24 +26,51 @@ public class AddTwoNumber{
                 p2 = p2.next;
             }
 
-            if(sum>9){
-                carry=1;
-                sum = sum-10;
-            }else{
-                carry = 0;
-            }
+            carry = sum / 10;
 
-            ListNode l = new ListNode(sum);
+            ListNode l = new ListNode(sum % 10);
             p.next = l;
             p = p.next;
         }
-
-        //don't forget check the carry value at the end
-        if(carry > 0){
-            ListNode l = new ListNode(carry);
-            p.next = l;
-        }
         return fake.next;
+    }
+
+    public boolean CompareLists(ListNode l1,ListNode l2){
+        while (l1 != null && l2 != null && l1.val == l2.val){
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+
+        if (l1   == null && l2 == null){
+            return true;
+        }
+        return false;
+    }
+
+    public ListNode MakeListNode(int[] arr){
+        ListNode listNode = new ListNode(arr[0]);
+        ListNode p = listNode;
+        for (int i = 1;i < arr.length;i++){
+            p.next = new ListNode(arr[i]);
+            p = p.next;
+        }
+        return listNode;
+    }
+
+    public static void main(String[] args){
+        AddTwoNumber addTwoNumber = new AddTwoNumber();
+        int arr1[] = new int[]{1,3,4,5};
+        int arr2[] = new int[]{4,5,6};
+        int arr3[] = new int[]{5,8,0,6};
+        ListNode ln1 = addTwoNumber.MakeListNode(arr1);
+        ListNode ln2 = addTwoNumber.MakeListNode(arr2);
+        ListNode ln3 = addTwoNumber.MakeListNode(arr3);
+
+        ListNode ans = addTwoNumber.addTwoNumbers(ln1,ln2);
+
+        Boolean bool = addTwoNumber.CompareLists(ans,ln3);
+        System.out.println(bool);
+
     }
 }
 
