b8a74a81cd1155d58e9745bbfcb67fc6576bf83f
shuang zhang <stevenshuang521@gmail.com>
2020-01-17 00:04:33 +0800

    delete node in a linked list test, container my ideas

---
+++ zs-leetcode-go/DeleteNodeInALinkedList/deletNodeInALinkedList_test.go
@@ -0,0 +1,32 @@
+package  DeleteNodeInALinkedList
+
+import (
+    "fmt"
+    "testing"
+    "for-an-offer/zs-leetcode-go/types"
+
+)
+
+func TestDeleteNode(t *testing.T) {
+    list := &types.ListNode{
+        1, nil,
+    }
+    second := &types.ListNode{
+        2, nil,
+    }
+    th3 := &types.ListNode {
+        3, nil,
+    }
+    second.Next = th3
+    list.Next = second
+    fmt.Println(list,"  ",  second, &second, "   ", th3) // test address
+    list.Display()
+    tmpDeleteNode(&second)
+    fmt.Println(second.Val)
+    list.Display()
+}
+
+func tmpDeleteNode(node **types.ListNode) {
+    next := (*node).Next
+    *node = next
+}
