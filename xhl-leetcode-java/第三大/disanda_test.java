2fbe06dcf7b75f92320947a2e742a5274a663f62
ZSZSZSZSZSZSFATHER <563055683@qq.com>
2020-03-30 10:35:28 +0800

    第三大

---
+++ xhl-leetcode-java/第三大/disanda_test.java
@@ -0,0 +1,13 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class disanda_test {
+    public static void main(String[] args) {
+        disanda obj=new disanda();
+        int [] nums={3,2,4};
+        System.out.println(obj.thirdMax(nums));
+    }
+
+
+
+
+}
