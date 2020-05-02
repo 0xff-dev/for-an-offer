9e63adaff48e3b294a9605bfe0edd126b27dc72b
shuang zhang <stevenshuang521@gmail.com>
2020-05-02 18:00:18 +0800

    word break

--- zs-leetcode-go/WordBreak/WordBreak.go
+++ zs-leetcode-go/WordBreak/WordBreak.go
@@ -1,11 +1,44 @@
 package WordBreak
 
-import "bytes"
+import (
+	"strings"
+)
 
 func wordBreak(s string, wordDict []string) bool {
-	buf := bytes.NewBufferString("")
-	for index := 0; index < len(wordBreak); index = (index + 1) % len(wordBreak) {
+	checkMap := make(map[int]bool)
+	return aux(s, 0, wordDict, checkMap)
+}
 
+func aux(s string, index int, words []string, checkMap map[int]bool) bool {
+	if index == len(s) {
+		checkMap[index] = true
+		return true
+	}
+	if val, ok := checkMap[index]; ok {
+		return val
+	}
+	for _, word := range words {
+		if strings.HasPrefix(s[index:], word) && aux(s, index+len(word), words, checkMap){
+			checkMap[index] = true
+			return true
+		}
+	}
+	checkMap[index] = false
+	return false
+}
+
+/*
+func wordBreak(s string, wordDict []string) bool {
+	if len(s) == 0 {
+		return true
+	}
+	for _, word := range wordDict {
+		var res bool
+		if strings.HasPrefix(s, word) {
+			res = wordBreak(s[len(word):], wordDict)
+			if res { return true}
+		}
 	}
 	return false
 }
+*/
