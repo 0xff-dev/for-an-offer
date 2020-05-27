00be12154ad40bbae7af0419092a949f66e8a8d3
shuang zhang <stevenshuang521@gmail.com>
2020-05-27 23:14:29 +0800

    scroll array

--- zs-leetcode-go/LongestCommonSubsequence/LongestCommonSubsequence.go
+++ zs-leetcode-go/LongestCommonSubsequence/LongestCommonSubsequence.go
@@ -25,3 +25,27 @@ func longestCommonSubsequence(text1 string, text2 string) int {
 	}
 	return dp[fmt.Sprintf(keyFormat, len1-1, len2-1)]
 }
+
+// 滚动数组
+func longestCommonSubsequence1(text1 string, text2 string) int {
+	len1, len2 := len(text1), len(text2)
+	if len1 == 0 || len2 == 0 {
+		return 0
+	}
+	dp := [2][]int{}
+	for index := 0; index < 2; index ++ {
+		dp[index] = make([]int, len2+1)
+	}
+	scrollWheel := 0
+	for index := 1; index <= len1; index++ {
+		for inner := 1; inner <= len2; inner ++ {
+			if text1[index-1] == text2[inner-1] {
+				dp[scrollWheel][inner] = dp[(scrollWheel+1)%2][inner-1] + 1
+			} else {
+				dp[scrollWheel][inner] = max(dp[(scrollWheel+1)%2][inner], dp[scrollWheel][inner-1])
+			}
+		}
+		scrollWheel = (scrollWheel+1)%2
+	}
+	return dp[(len1+1)%2][len2]
+}
