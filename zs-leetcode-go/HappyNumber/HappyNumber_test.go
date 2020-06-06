97d97232e5656934a6719b02fa051c1e689e3336
shuang zhang <stevenshuang521@gmail.com>
2020-06-06 21:14:09 +0800

    happy number test

---
+++ zs-leetcode-go/HappyNumber/HappyNumber_test.go
@@ -0,0 +1,14 @@
+package HappyNumber
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestIsHappy(t *testing.T) {
+	//fmt.Println(isHappy(19))
+	//fmt.Println(isHappy(1000))
+	//fmt.Println(isHappy(1))
+	//fmt.Println(isHappy(99998))
+	fmt.Println(isHappy(2))
+}
