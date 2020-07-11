fafbaf19e13224ba9c55898cc23d6735aa8dca60
shuang zhang <stevenshuang521@gmail.com>
2020-07-11 22:01:09 +0800

    binary search

---
+++ zs-leetcode-go/BinarySearch/BinarySearch.go
@@ -0,0 +1,16 @@
+package BinarySearch
+
+func search(nums []int, target int) int {
+	start, end := 0, len(nums)-1
+	for start <= end {
+		mid := start + (end-start)/2
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] < target {
+			start = mid+1
+		} else {
+			end = mid-1
+		}
+	}
+	return -1
+}
